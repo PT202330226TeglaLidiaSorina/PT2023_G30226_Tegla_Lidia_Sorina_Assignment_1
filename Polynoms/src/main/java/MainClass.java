7c446c5aa4cc39ded05260a1aeed2b46cd8d6cf0
sorinalidia <126770993+sorinalidia@users.noreply.github.com>
2023-03-28 17:19:42 +0300

    Delete Polynoms directory

--- Polynoms/src/main/java/MainClass.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-public class MainClass {
-    public static void main(String[] args){
-        PolynomialCalculator calculator = new PolynomialCalculator();
-        calculator.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        calculator.setSize(600, 400);
-        calculator.setVisible(true);
-    }
-}
